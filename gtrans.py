55526aa06d1ac657f68f6762011847da252d81d6
reycn <oyrx@vip.qq.com>
2021-02-23 14:32:03 +0800

    Modify

--- gtrans.py
+++ gtrans.py
@@ -10,8 +10,10 @@ from google_trans_new import google_translator
 from termcolor import cprint
 from time import sleep
 
-def text_clean(text):
+
+def clean_text(text):
     # TODO: 文本清洗
+    # print(text)
     text = re.sub('(\[转发自.*\])\n', '', text)
     text = text.replace('\n', '/////')
     text = text.replace('#', ' ')
@@ -40,12 +42,18 @@ def big5(text):
 
 
 def trans(text, lang='zh-CN', detect=1):
-    text = text_clean(text)
+    text = clean_text(text)
     tr = google_translator()
     if lang == 'en':
-        result =get_trans(text, lang_tgt='en')
+        result = get_trans(text, lang_tgt='en')
     elif lang == 'zh':
         result = get_trans(text, lang_tgt='zh-CN')
+    elif lang == 'ru':
+        result = get_trans(text, lang_tgt='ru')
+    elif lang == 'ja':
+        result = get_trans(text, lang_tgt='ja')
+    elif lang == 'vi':
+        result = get_trans(text, lang_tgt='vi')
     else:
         if get_lang(text)[0] == 'zh-CN':
             result = get_trans(text, lang_tgt='zh-CN') + '\n' \
@@ -55,6 +63,20 @@ def trans(text, lang='zh-CN', detect=1):
                 + text
     return result
 
+
+def trans_auto(text):
+    text = clean_text(text)
+    tr = google_translator()
+    if get_lang(text)[0] == 'zh-CN':
+        result = get_trans(text, lang_tgt='en')
+    elif get_lang(text)[0] == 'en':
+        result = get_trans(text, lang_tgt='zh-CN')
+    else:
+        result = get_trans(text, lang_tgt='zh-CN') + '\n\n' + get_trans(
+            text, lang_tgt='en')
+    return result
+
+
 def get_lang(text):
     translator = google_translator()
     lang = None
@@ -63,28 +85,30 @@ def get_lang(text):
             lang = translator.detect(text)
         except:
             translator = Translator()
-            sleep(0.8)
+            sleep(0.1)
             pass
     return lang
 
-result = get_lang('hello')
 
-def get_trans(text,**kwargs):
+# result = get_lang('hello')
+
+
+def get_trans(text, **kwargs):
     translator = google_translator()
     result = None
     while result == None:
         try:
-            result = translator.translate(text,**kwargs)
+            result = translator.translate(text, **kwargs)
         except Exception as e:
             cprint('API Error' + str(e), 'white', 'on_yellow')
             translator = google_translator()
-            sleep(0.8)
+            sleep(0.1)
             pass
-    return result     
-    
-result = get_trans('hello',lang_tgt='ja') 
+    return result
+
+
+# result = get_trans('hello',lang_tgt='ja')
 
 if __name__ == "__main__":
+    print('Please run main.py instead of me!')
     pass
-
-
